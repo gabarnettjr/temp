9a88db3c4d8104f07bd637ca834baa9a4ae729b9
gabarnettjr <gabarnettjr@gmail.com>
2016-08-09 09:46:35 -0600

    Update fluxTest.m

--- fluxTest.m
+++ fluxTest.m
@@ -1,7 +1,7 @@
-clc,clear
+function fluxTest
 
 stencilSize = 6;
-phi = @(x,y) (x.^2+y.^2).^(3/2);
+phi = @(x,y)  (x.^2+y.^2) .^ (3/2);
 
 n = 5;
 h = 1/(n-2);
@@ -12,4 +12,16 @@ xV = xV(:);  yV = yV(:);
 [xH,yH] = meshgrid( h/2:h:1-h/2, 0:h:1 );
 xH = xH(:);  yH = yH(:);
 
-idx = knnsearch( [x,y], [xV,yV], 'k', stencilSize );
+idxV = knnsearch( [x,y], [xV,yV], 'k', stencilSize );
+idxH = knnsearch( [x,y], [xH,yH], 'k', stencilSize );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function W = quadMatrixV( phi, x, y, idxV, xV, yV )
+W = zeros( size(idxV,2), length(xV) );
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function W = quadMatrixH( phi, x, y, idxH, xH, yH )
+W = zeros( size(idxH,2), length(xH) );
