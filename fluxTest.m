3b4f1f064bd20125fa66089f7a65fbf3d6b78a6d
gabarnettjr <gabarnettjr@gmail.com>
2016-08-09 09:32:52 -0600

    Update fluxTest.m

--- fluxTest.m
+++ fluxTest.m
@@ -1,4 +1,12 @@
 clc,clear
 
 n = 5;
-[xx,yy] = meshgrid(linspace(0,1,n));
+h = 1/(n-2);
+[xx,yy] = meshgrid( -h/2 : h : 1+h/2 );
+x = xx(:);  y = yy(:);
+[xV,yV] = meshgrid( 0:h:1, h/2:h:1-h/2 );
+xV = xV(:);  yV = yV(:);
+[xH,yH] = meshgrid( h/2:h:1-h/2, 0:h:1 );
+xH = xH(:);  yH = yH(:);
+
+idx = knnsearch( [x,y], [xV,yV], 'k', stencilSize );
